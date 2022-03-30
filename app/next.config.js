2ffbe5ff9cd233b46b19265a15c11166286a62c6
Kevin Forrest Connors <kevinforrestconnors@gmail.com>
2022-03-30 13:54:09 -0400

    connect to jupiter

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      stream: false,
+      os: false,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,23 @@
 import type { NextPage } from "next";
 import bs58 from "bs58";
-import fetch from "node-fetch";
 import Head from "next/head";
-import Image from "next/image";
-import { Connection, PublicKey, Keypair } from "@solana/web3.js";
-import { Jupiter, RouteInfo, TOKEN_LIST_URL } from "@jup-ag/core";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
+
+import { TOKEN_LIST_URL } from "@jup-ag/core";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
+interface Token {
+  chainId: number; // 101,
+  address: string; // 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+  symbol: string; // 'USDC',
+  name: string; // 'Wrapped USDC',
+  decimals: number; // 6,
+  logoURI: string; // 'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/BXXkv6z8ykpG1yuvUDPgh732wzVHB69RnB9YgSYh3itW/logo.png',
+  tags: string[]; // [ 'stablecoin' ]
+}
 
 const ThemeChanger = () => {
   const [mounted, setMounted] = useState(false);
@@ -36,10 +45,33 @@ const Home: NextPage = () => {
   const wallet = useWallet();
 
   const [connected, setConnected] = useState(false);
+  const [tokens, setTokens] = useState<Token[]>([]);
+  useEffect(() => {
+    // Fetch token list from Jupiter API
+    const network = (process.env.network ||
+      WalletAdapterNetwork.Devnet) as WalletAdapterNetwork;
+    fetch(TOKEN_LIST_URL[network])
+      .then((response) => response.json())
+      .then((result) => setTokens(result as Token[]));
+  }, []);
 
   useEffect(() => {
     setConnected(wallet.connected);
   }, [wallet]);
+
+  console.log(tokens);
+
+  function Tokens(tokens: Token[]): React.ReactElement {
+    let e: ReactElement[] = [];
+
+    tokens.forEach((t: Token) => {
+      e.push(
+        <div>{`Symbol: ${t.symbol}, Address: ${t.address}, Name: ${t.name}, Decimals: ${t.decimals}`}</div>
+      );
+    });
+
+    return <>{e}</>;
+  }
 
   return (
     <>
@@ -54,6 +86,7 @@ const Home: NextPage = () => {
           <h2>
             {connected ? "Wallet is connected." : "Wallet is not connected"}
           </h2>
+          <h2>Tokens: {Tokens(tokens)}</h2>
           <div>
             <ThemeChanger />
           </div>
